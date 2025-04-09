2c7165ca733c2f2167bfb1b7be6def83cbe44fb5
Mauro Felix <119742741+mauro-fe@users.noreply.github.com>
2025-04-08 22:16:46 -0300

    Adicionando projeto

---
+++ script.js
@@ -0,0 +1,24 @@
+// COMEÇO HEADER
+// Fazer scroll suave
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 10) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+// FIM HEADER
+
+// COMEÇO GALERIA
+// Função para abrir a imagem da galeria
+function openImage(src) {
+    document.getElementById('overlay').style.display = 'flex';
+    document.getElementById('overlayImg').src = src;
+}
+// Função para fechar a imagem da galeria
+function closeImage() {
+    document.getElementById('overlay').style.display = 'none';
+}
+// FIM GALERIA
